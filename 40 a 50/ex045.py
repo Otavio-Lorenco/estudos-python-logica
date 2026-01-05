db33b802535fe4315d969b2612f5fc436d1c070d
Otavio-Lorenco <olouremco@gmail.com>
2026-01-05 13:53:40 -0300

    Add files via upload

---
+++ 40 a 50/ex045.py
@@ -0,0 +1,4 @@
+A = int(input('digite um numero para a variavel A: '))
+B = int(input('digite um numero para a variavel B: ')) 
+c =  (A ** 2) + (B ** 2)
+print (c)
---
+++ 40 a 50/ex048.py
@@ -0,0 +1,15 @@
+# Write code below 💖
+prato = input('Qual foi o prato que voce comeu? ')
+nota = float(input('De uma nota de 0 a 5 para a comida '))
+print (nota)
+
+if nota >= 4.5:
+  print ('Extraordinario ⭐⭐⭐⭐⭐')
+elif nota >= 4:
+  print ('excelente ⭐⭐⭐⭐')  
+elif nota >= 3:
+  print ('Bom ⭐⭐⭐')
+elif nota >= 2:
+  print ('Justo ⭐⭐')
+else:
+  print ('Horrivel ⭐')   
