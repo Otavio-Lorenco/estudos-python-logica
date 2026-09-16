b13388968404dcdccfa6786f2e6acf59dbab58bd
Otavio-Lorenco <olouremco@gmail.com>
2026-09-16 16:00:08 -0300

    att

--- 40 a 50/ex044.py
+++ 40 a 50/ex044.py
@@ -1,10 +1,10 @@
 #aqui criamos um arquivo txt
 arquivo = open('ex044.txt', 'w')
 
-arquivo.write('aqui vai criar um arquivo de texto dentro da nossa pasta \n ')
+arquivo.write('aqui vai criar um arquivo de texto dentro da nossa pasta, eu quero que esta escrito que o corinthians e campeao mundial\n ')
 arquivo.close()
 
 #agora vamo fazer a maquina ler
 leitura = open('ex044.txt', 'r')
 print (leitura.read())
-leitura.close
+leitura.close()
---
+++ 51 a 60/ex051.py
@@ -0,0 +1,19 @@
+contato = {
+    "nome": nome,
+    "idade": idade,
+    "email": email,
+    "telefone": telefone
+}
+
+def criar_contato ():
+    nome = input('Digite o seu nome aqui: ')
+    idade = input('Digite a sua idade aqui: ')
+    email = input('digite o seu email aqui: ')
+    telefone = input('Digite seu numero de telefone aqui: ')
+
+
+def mostrar_contato ():
+    print('nome: ', nome)
+    print('idade: ', idade)
+    print('email: ', email )
+    print('telefone: ', telefone)
